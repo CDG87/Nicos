70bd22a3da90ee92d07caf20b192db609ad063ba
Killian <fourgeaud.killian@gmail.com>
2018-01-09 13:06:32 +0100

    controleur maj

---
+++ controleur/c_maj.php
@@ -0,0 +1,17 @@
+<?php
+
+$action = $_REQUEST['action'];
+$_SESSION['entpied'] = "";
+
+
+switch($action) {	
+	case 'selectionner_critere' : 
+		break;
+	case 'coordonees_structures' :
+		break;
+	case 'coordonees_inspecteur' : 
+		break;
+	case 'logo_adresse' : 
+		break;
+}
+?>
