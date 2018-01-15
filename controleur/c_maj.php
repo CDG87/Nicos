f8b9591653db1783851602bd7a338f7a2bf79aea
Killian <fourgeaud.killian@gmail.com>
2018-01-15 11:36:43 +0100

    Le controlleur n'est pas selectionner par defaut a la creation d'un audit

--- controleur/c_maj.php
+++ controleur/c_maj.php
@@ -78,10 +78,8 @@ switch($action) {
 			$pdo->enregistrer_Controleur($_POST['nom_inspecteur'], $_POST['prenom_inspecteur'], $_POST['fonction_inspecteur'], $_POST['affectation_inspecteur'], $_POST['centre_inspecteur'], $_POST['adresse_inspecteur'], $_POST['tel_fixe_inspecteur'], $_POST['tel_portable_inspecteur'], $_POST['fax_inspecteur'], $_POST['email_inspecteur'] );
 			header('Location:index.php?uc=maj&action=coordonees_inspecteur');
 		}
-		if(isset($_POST['choix_inspecteur'])){
-			$dispo="";
-		}
 		break;
+
 	case 'logo_adresse' : 
 		break;
 }
