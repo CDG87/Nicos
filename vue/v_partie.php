23e9c9441a31474ee8d80ac10024400b7a9d0055
Thomas Crouvizier <tcrouvizier@gmail.com>
2018-02-01 13:31:56 +0100

    a

--- vue/v_partie.php
+++ vue/v_partie.php
@@ -5,12 +5,12 @@ include("include/entete.php");
     <tr>
         <td class="espart">
             <form method='POST' action='index.php?uc=organisationnel&action=theme&nt=oui&entpied=theme'>
-                <button type="submit" class="btn btn-danger btn-radius btn-partie" name="codepartie" value="1" >Organisationnel</button>
+                <button type="submit" class="btn btn-danger btn-partie" name="codepartie" value="1" >Organisationnel</button>
             </form>
         </td>
         <td class="espart">
             <form method='POST' action='index.php?uc=sursite&action=pole&nt=oui&entpied=pole'>
-                <button type="submit" class="btn btn-danger btn-radius btn-partie" name="codepartie" value="2" >Sur site</button>
+                <button type="submit" class="btn btn-danger btn-partie" name="codepartie" value="2" >Sur site</button>
             </form>
         </td>
     </tr>
