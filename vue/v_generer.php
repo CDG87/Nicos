d7cd7d8ba59aff7bd734cca7034fcb6ce21e6c0e
Killian <fourgeaud.killian@gmail.com>
2018-02-01 11:17:16 +0100

    btn-generer-rond

--- vue/v_generer.php
+++ vue/v_generer.php
@@ -3,11 +3,11 @@
 	<table>
 			<tr>
 				<form method="post" action="index.php?uc=rapport&action=generer">
-					<td><input class="btn btn-art btn-primary" type="submit" value="Générer rapport" /></td>
+					<td><input class="btn btn-generer-rond btn-primary" type="submit" value="Générer rapport" /></td>
 				</form>
 				<?php if($infosaudit['LIBELLE_AUDIT']=="Inspection"){ ?>
 				<form method="post" action="index.php?uc=rapport&action=genererMesure">
-					<td><input class="btn btn-art btn-primary" id="mesureUrg" type="submit" value="Mesures Urgentes" /></td>
+					<td><input class="btn btn-generer-rond btn-primary" id="mesureUrg" type="submit" value="Mesures Urgentes" /></td>
 				</form>
 				<?php } ?>
 			</tr>
