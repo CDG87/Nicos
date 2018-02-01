bb250c7a4493d27e0f63f68ae2546b936a155d28
Thomas Crouvizier <tcrouvizier@gmail.com>
2018-02-01 11:26:50 +0100

    a

--- vue/v_generer.php
+++ vue/v_generer.php
@@ -7,7 +7,7 @@
 				</form>
 				<?php if($infosaudit['LIBELLE_AUDIT']=="Inspection"){ ?>
 				<form method="post" action="index.php?uc=rapport&action=genererMesure">
-					<td><input class="btn btn-generer-rond btn-primary" id="mesureUrg" type="submit" value="Mesures Urgentes" /></td>
+					<td><input class="btn btn-generer-rond btn-primary" type="submit" value="Mesures Urgentes" /></td>
 				</form>
 				<?php } ?>
 			</tr>
